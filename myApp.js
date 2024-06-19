3a9ae14518604029a240a4f7dd9f54bb16c384c7
Nadeesh Weerarathna <nadeesh-cs20024@stu.kln.ac.lk>
2024-06-19 08:08:00 +0530

    Serve an HTML File

--- myApp.js
+++ myApp.js
@@ -1,15 +1,29 @@
 let express = require('express');
+const path = require('path');
 let app = express();
 
-app.get("/", (request, response) => {
+// app.get("/", (request, response) => {
 
-    console.log("Hello World");
-    return response.send("Hello Express");
-}
-);
+//     console.log("Hello World");
+//     return response.send("Hello Express");
+// }
+// );
 
 
-
+app.get('/',  function(req, res){
+    const options = {
+        root : path.join(__dirname, 'views' )
+    };
+    const fileName = "index.html";
+    res.sendFile(fileName, options, function(err){
+        if(err){
+            console.log('Error Sending file:', err);
+        }
+        else{
+            console.log('Sent:', fileName);
+        }
+    })
+})
 
 
 
